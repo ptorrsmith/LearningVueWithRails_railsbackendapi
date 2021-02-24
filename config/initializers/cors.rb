4a786e46df6387c638dfb75dfc6b10aa29c59fe6
Peter Torr Smith <peter@ptorrsmith.com>
2021-02-24 15:56:49 +1300

    enable and configure cors initializer with permitted origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,18 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # origins 'example.com'
+    origins 'http://localhost:8080'  # allows our front-end vue app, served from another source (in this case :8080) to communicate with our server
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
+
+
+# localhost:3000 is rails server
+# localhost:8080 is the front-end
+
